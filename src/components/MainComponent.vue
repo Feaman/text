643d14a0d55f95cc6e8562ed586b726d83683911
Pavel Aksyonov <elf.feaman@gmail.com>
2023-09-05 15:25:28 +0300

    Commit

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,46 +1,63 @@
 <template>
   <div class="container">
     <div
-      :style="{ fontSize: `${fontSize}px`, justifyContent: textAlign }"
-      ref="textarea"
-      class="textarea"
-      placeholder="Начните вводить текст"
-      contenteditable="true"
+      @click="textareaContainerOnClick"
+      class="textarea-container"
+      ref="textareaContainer"
     >
-      {{ text }}
+      <textarea
+        v-model="text"
+        @input.stop="handleListItemTextAreaHeight"
+        :style="{ fontSize: `${fontSize}px`, textAlign: textAlign as 'center' }"
+        ref="textarea"
+        class="textarea"
+        placeholder="Начните вводить текст"
+      />
     </div>
-    <div class="tools">
-      <img 
-        @click="alignText('left')"
-        src="../assets/align-left.svg"
-        :class="['tool-icon', { 'tool-icon--active': textAlign === TEXT_ALIGN_LEFT }]"
-        alt="Align left"
-      />
-      <img 
-        @click="alignText('center')"
-        src="../assets/align-center.svg"
-        :class="['tool-icon', { 'tool-icon--active': textAlign === TEXT_ALIGN_CENTER }]"
-        alt="Align left"
-      />
-      <img 
-        @click="alignText('right')"
-        src="../assets/align-right.svg"
-        :class="['tool-icon', { 'tool-icon--active': textAlign === TEXT_ALIGN_RIGHT }]"
-        alt="Align left"
-      />
+    <Transition name="fade">
       <div
-        class="slider"
+        v-if="isToolsShown"
+        class="tools"
       >
-        <Slider
-          v-model="sliderValue"
-          @update="fontSize = $event"
-          :min="1"
-          :max="90"
-          showTooltip="drag"
-          :lazy="false"
+        <img 
+          @click="alignText('left')"
+          src="../assets/align-left.svg"
+          :class="['tool-icon', { 'tool-icon--active': textAlign === TEXT_ALIGN_LEFT }]"
+          alt="Align left"
         />
+        <img 
+          @click="alignText('center')"
+          src="../assets/align-center.svg"
+          :class="['tool-icon', { 'tool-icon--active': textAlign === TEXT_ALIGN_CENTER }]"
+          alt="Align left"
+        />
+        <img 
+          @click="alignText('right')"
+          src="../assets/align-right.svg"
+          :class="['tool-icon', { 'tool-icon--active': textAlign === TEXT_ALIGN_RIGHT }]"
+          alt="Align left"
+        />
+        <img 
+          @click="hideTools()"
+          src="../assets/fullscreen.svg"
+          :class="['tool-icon']"
+          style="fill: #bababa"
+          alt="Fullscreen"
+        />
+        <div
+          class="slider"
+        >
+          <Slider
+            v-model="sliderValue"
+            @update="changeFontSize"
+            :min="1"
+            :max="90"
+            showTooltip="drag"
+            :lazy="false"
+          />
+        </div>
       </div>
-    </div>
+    </Transition>
   </div>
 </template>
 
@@ -57,6 +74,7 @@ const fontSize = ref(16)
 const sliderValue = ref(fontSize.value)
 const textAlign = ref(TEXT_ALIGN_CENTER)
 const textarea = ref<HTMLTextAreaElement>()
+const isToolsShown = ref(true)
 
 // Fix 100vh for mobile
 document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`)
@@ -65,19 +83,12 @@ onMounted(() => {
   textarea.value?.focus()
   handleWindowResize()
   window.addEventListener('resize', handleWindowResize)
-
-  textarea.value?.addEventListener("paste", function(event: ClipboardEvent) {
-    event.preventDefault();
-
-    if (event.clipboardData) {
-      const content = event?.clipboardData.getData('text/plain');
-
-      if (textarea.value && content) {
-        textarea.value.innerText = content;
-      }
-    }
-});
 })
+
+function textareaContainerOnClick() {
+  textarea.value?.focus()
+  isToolsShown.value = true
+}
 
 function alignText(alignType: string) {
   textAlign.value = alignType
@@ -87,43 +98,66 @@ function handleWindowResize() {
   // Fix 100vh for mobile
   document.documentElement.style.setProperty('--vh', `${window.innerHeight * 0.01}px`)
 }
+
+function handleListItemTextAreaHeight() {
+  if (textarea.value) {
+    const $textArea = textarea.value
+    let textAreaHeight = 0
+    $textArea.style.height = '0'
+    textAreaHeight = $textArea.scrollHeight
+    $textArea.style.height = `${textAreaHeight}px`
+  }
+}
+
+function changeFontSize(newFontSize: number) {
+  fontSize.value = newFontSize
+  setTimeout(() => {
+    handleListItemTextAreaHeight()
+  });
+}
+
+function  hideTools() {
+  isToolsShown.value = false
+}
 </script>
 
 <style scoped>
 .container {
   width: 100%;
   height: calc(var(--vh, 1vh) * 100);
+  max-height: calc(var(--vh, 1vh) * 100);
   display: flex;
   flex-direction: column;
   background-color: #fff;
 }
 
+.textarea-container {
+  width: 100%;
+  max-height: 100%;
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: auto;
+}
+
 .textarea {
   width: 100%;
-  height: calc(100% - 64px);
-  display: flex;
-  flex-direction: column;
+  max-height: 100%;
   font-family: Roboto, system-ui, Avenir, Helvetica, Arial, sans-serif;
   border: none;
   resize: none;
   margin: 0;
   padding: 15px;
   outline: 0;
-  overflow: auto;
   background-color: #fff;
   color: #000;
-  align-items: center;
-}
-
-[contenteditable=true]:empty:before{
-  content:attr(placeholder);
-  color: #ccc;
-  font-size: 24px;
 }
 
 .tools {
   width: 100%;
   height: 64px;
+  min-height: 64px;
   display: flex;
   align-items: center;
   box-shadow: 0 0 5px #000;
